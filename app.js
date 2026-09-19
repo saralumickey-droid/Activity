c81bc5b8c3a6fb2688374052082f55c684b0455b
Catarinaaa <sara.lu.mickey@gmail.com>
2026-09-19 10:22:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ const app = express();
 app.use(cors()); 
 app.use(express.json());
 
-// Conexión a la base de datos usando las variables de entorno
-const db = await mysql.createConnection({
+// Conexión mediante Pool de conexiones
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 // Rutas API Clientes
