ba449778b1bb9a3a3ad14200987a73bc159b3424
Catarinaaa <sara.lu.mickey@gmail.com>
2026-09-19 10:34:45 -0500

    Add Menu component with navigation bar

---
+++ menu.jsx
@@ -0,0 +1,22 @@
+src/components/menu.jsx
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+function Menu() {
+  return (
+    <Navbar style={{ backgroundColor: '#67001a' }} variant="dark" expand="lg" sticky="top">
+      <Container>
+        <Navbar.Brand href="#">Tienda Ej</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#clientes">Clientes</Nav.Link>
+            <Nav.Link href="#productos">Productos</Nav.Link>
+            <Nav.Link href="#ventas">Ventas</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Menu;
