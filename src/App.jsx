9d8860a57d5cef2e7359d4591decf60380216c63
Catarinaaa <sara.lu.mickey@gmail.com>
2026-09-26 08:30:13 -0500

    Implement routing with React Router in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
+import { Routes, Route } from 'react-router-dom';
 import Menu from './components/menu';
 import Clientes from './components/Clientes';
 import Productos from './components/Productos';
@@ -9,23 +10,22 @@ function App() {
   return (
     <>
       <Menu />
+
       <div className="container mt-4 mb-5">
-        <h1 className="text-center mb-4">Panel Principal - Tienda Ej</h1>
-        
-        {}
-        <section className="section-card" id="clientes">
-          <Clientes />
-        </section>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <h1 className="text-center mb-4">
+                Panel Principal - Tienda Ej
+              </h1>
+            }
+          />
 
-        {}
-        <section className="section-card" id="productos">
-          <Productos />
-        </section>
-
-        {}
-        <section className="section-card" id="ventas">
-          <Ventas />
-        </section>
+          <Route path="/clientes" element={<Clientes />} />
+          <Route path="/productos" element={<Productos />} />
+          <Route path="/ventas" element={<Ventas />} />
+        </Routes>
       </div>
     </>
   );
