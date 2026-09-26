7f1f587e9d83894367d96cae7f1d561239594cd3
Catarinaaa <sara.lu.mickey@gmail.com>
2026-09-26 08:37:25 -0500

    Refactor Menu component for improved readability

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -3,7 +3,12 @@ import { Link } from 'react-router-dom';
 
 function Menu() {
   return (
-    <Navbar style={{ backgroundColor: '#67001a' }} variant="dark" expand="lg" sticky="top">
+    <Navbar
+      style={{ backgroundColor: '#67001a' }}
+      variant="dark"
+      expand="lg"
+      sticky="top"
+    >
       <Container>
         <Navbar.Brand as={Link} to="/">
           Tienda Ej
@@ -13,9 +18,17 @@ function Menu() {
 
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/clientes">Clientes</Nav.Link>
-            <Nav.Link as={Link} to="/productos">Productos</Nav.Link>
-            <Nav.Link as={Link} to="/ventas">Ventas</Nav.Link>
+            <Nav.Link as={Link} to="/clientes">
+              Clientes
+            </Nav.Link>
+
+            <Nav.Link as={Link} to="/productos">
+              Productos
+            </Nav.Link>
+
+            <Nav.Link as={Link} to="/ventas">
+              Ventas
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
