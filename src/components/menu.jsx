2cd1ec02301361f9b57571e1af84017b0713577b
Catarinaaa <sara.lu.mickey@gmail.com>
2026-09-26 08:30:27 -0500

    Update Navbar links to use React Router

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,17 +1,21 @@
-
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Menu() {
   return (
     <Navbar style={{ backgroundColor: '#67001a' }} variant="dark" expand="lg" sticky="top">
       <Container>
-        <Navbar.Brand href="#">Tienda Ej</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          Tienda Ej
+        </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="clientes">Clientes</Nav.Link>
-            <Nav.Link href="productos">Productos</Nav.Link>
-            <Nav.Link href="ventas">Ventas</Nav.Link>
+            <Nav.Link as={Link} to="/clientes">Clientes</Nav.Link>
+            <Nav.Link as={Link} to="/productos">Productos</Nav.Link>
+            <Nav.Link as={Link} to="/ventas">Ventas</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
